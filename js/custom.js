bf1df23da67a58ba17b7d24f61cc3219882bce4a
truong <truongqv13@gmail.com>
2018-07-09 14:11:55 +0700

    fix menu

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,5 @@
 $(document).ready(function($) {
 	new WOW().init();
-	$('.carousel[data-type="multi"] .item').each(function() {
-		var next = $(this).next();
-		if (!next.length) {
-			next = $(this).siblings(':first');
-		}
-		next.children(':first-child').clone().appendTo($(this));
-
-		for (var i = 0; i < 2; i++) {
-			next = next.next();
-			if (!next.length) {
-				next = $(this).siblings(':first');
-			}
-
-			next.children(':first-child').clone().appendTo($(this));
-		}
-	});
 
     $(".images").hover( function (e) {
         $(this).find('img').toggleClass('pulse animated infinite', e.type === 'mouseenter');
