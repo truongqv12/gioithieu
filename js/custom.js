da636e7689979da8c724ce098c7587134eec66e0
truongtt <truongtt@vatgia.com>
2018-08-01 09:26:28 +0700

    fixx8

--- js/custom.js
+++ js/custom.js
@@ -4,14 +4,27 @@ $(document).ready(function($) {
     $(".images").hover( function (e) {
         $(this).find('img').toggleClass('pulse animated infinite', e.type === 'mouseenter');
     });
+    // Scroll to page sections
+    function goToByScroll(id){
+        // Reove "link" from the ID
+        id = id.replace("link-", "");
+        // Scroll
+        $('html,body').animate({
+                scrollTop: $("#"+id).offset().top},
+            'slow');
+    }
 
+    $("nav").find("a").click(function() {
+        // Call the scroll function
+        goToByScroll($(this).attr("id"));
+    });
     // GÂU TU TỐP
 	$("a.btn_go_top").click(function() {
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 	    return false;
 	});
 	$("a.btn_go_top").hover( function (e) {
-		$(this).toggleClass('pulse bounce');
+		$(this).toggleClass('pulse tada');
     });
 	$(window).scroll(function(e) {
 		var Scroll_top = $(window).scrollTop();
